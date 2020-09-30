0f710c5411b3a99167ee25b3482a4cb4c86f020a
zabir-nabil <zabiralnazi@yahoo.com>
2020-09-30 16:33:20 +0600

    added

--- client_lp_bb.py
+++ client_lp_bb.py
@@ -54,75 +54,134 @@ image_path = ["C:/Users/Zabir/Documents/test_train/test_train/train/jpg/*.jpg", 
 #
 image_path = ["D:/dhaka-ai_competition/train/Final Train Dataset/*.jpg", "D:/dhaka-ai_competition/test1/test/*.jpg"]
 
+image_path = ["../lp_videos/26.mp4", "../lp_videos/out.mp4", "../lp_videos/2.mp4", "../lp_videos/vid3.avi"] # video paths
+
 
 # image_path = ["*.jpg", "*.jpg"] # dummy tester
 save_folder_tag = ["../dump/semiclean1", "../dump/clean1"] # inside dump folder
 save_folder_tag = ["../dump/semiclean2", "../dump/semiclean3"]
 
 save_folder_tag = ["../dump/noisy1", "../dump/noisy2"]
+
+
+save_folder_tag = ["../dump/vid1", "../dump/vid2", "../dump/vid3", "../dump/vid4"]
+
+video = 1 # video or folder
+
 img_cnt = 0
 
 
 
-
-
-
 for i, i_path in enumerate(image_path):
-    img_files = sorted(glob.glob(i_path))
-    frames_names = [a for a in img_files]
+
+    if not video:
+        img_files = sorted(glob.glob(i_path))
+        frames_names = [a for a in img_files]
+    else:
+        vid_cap = cv2.VideoCapture(i_path)
 
     s = Session()
 
     
+    if not video:
+        for ori_frame_name in tqdm(frames_names):
+            try:
+                ori_frame = cv2.imread(ori_frame_name)
 
-    for ori_frame_name in tqdm(frames_names):
-        try:
-            ori_frame = cv2.imread(ori_frame_name)
-
-            frame = image_resize(ori_frame, width = 416)
-            nf_x = ori_frame.shape[1] / frame.shape[1] # normalizing factor to convert co-ordinates from small frame to actual high-res frame
-            nf_y =  ori_frame.shape[0] / frame.shape[0]
+                frame = image_resize(ori_frame, width = 416)
+                nf_x = ori_frame.shape[1] / frame.shape[1] # normalizing factor to convert co-ordinates from small frame to actual high-res frame
+                nf_y =  ori_frame.shape[0] / frame.shape[0]
 
 
-            data = base64.b64encode(frame)
+                data = base64.b64encode(frame)
 
-            # r = requests.post("http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
+                # r = requests.post("http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
 
-            req = Request('POST', "http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
+                req = Request('POST', "http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
 
-            prepped = req.prepare()
+                prepped = req.prepare()
 
-            r = s.send(prepped)
+                r = s.send(prepped)
 
-            out = ast.literal_eval(json.loads(r.json())['data'])  # json.loads(r.json())['data']
-            
-            # print(out)
+                out = ast.literal_eval(json.loads(r.json())['data'])  # json.loads(r.json())['data']
+                
+                # print(out)
 
-            lp_cnt = 0
-            # print(out)
-            for obj in out:
+                lp_cnt = 0
+                # print(out)
+                for obj in out:
 
-                if obj[0] == 'lp':
-                    xc = obj[2][0]*nf_x
-                    yc = obj[2][1]*nf_y
-                    w = obj[2][2]*nf_x
-                    h = obj[2][3]*nf_y
-                    x1 = int(xc - w/2)
-                    x2 = int(xc + w/2)
-                    y1 = int(yc - h/2)
-                    y2 = int(yc + h/2)
-                    lp_cropped = ori_frame[y1:y2, x1:x2, :]
-                    try:
-                        origin = save_folder_tag[i].split("/")[-1]
-                        cv2.imwrite(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg", lp_cropped)
-                        # print(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg")
-                    except:
-                        pass
-                        # print("failed")
-                    lp_cnt += 1
+                    if obj[0] == 'lp':
+                        xc = obj[2][0]*nf_x
+                        yc = obj[2][1]*nf_y
+                        w = obj[2][2]*nf_x
+                        h = obj[2][3]*nf_y
+                        x1 = int(xc - w/2)
+                        x2 = int(xc + w/2)
+                        y1 = int(yc - h/2)
+                        y2 = int(yc + h/2)
+                        lp_cropped = ori_frame[y1:y2, x1:x2, :]
+                        try:
+                            origin = save_folder_tag[i].split("/")[-1]
+                            cv2.imwrite(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg", lp_cropped)
+                            # print(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg")
+                        except:
+                            pass
+                            # print("failed")
+                        lp_cnt += 1
 
-            img_cnt += 1
-        except:
-            time.sleep(1)
-            print('failed')
+                img_cnt += 1
+            except:
+                time.sleep(1)
+                print('failed')
+    else:
+        for i_f in tqdm(range(int(vid_cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
+            try:  
+                ret, ori_frame = vid_cap.read() 
 
+                frame = image_resize(ori_frame, width = 416)
+                nf_x = ori_frame.shape[1] / frame.shape[1] # normalizing factor to convert co-ordinates from small frame to actual high-res frame
+                nf_y =  ori_frame.shape[0] / frame.shape[0]
+
+
+                data = base64.b64encode(frame)
+
+                # r = requests.post("http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
+
+                req = Request('POST', "http://192.168.106.222:5000/predict_b64", data={'imgb64' : data})
+
+                prepped = req.prepare()
+
+                r = s.send(prepped)
+
+                out = ast.literal_eval(json.loads(r.json())['data'])  # json.loads(r.json())['data']
+                
+                # print(out)
+
+                lp_cnt = 0
+                # print(out)
+                for obj in out:
+
+                    if obj[0] == 'lp':
+                        xc = obj[2][0]*nf_x
+                        yc = obj[2][1]*nf_y
+                        w = obj[2][2]*nf_x
+                        h = obj[2][3]*nf_y
+                        x1 = int(xc - w/2)
+                        x2 = int(xc + w/2)
+                        y1 = int(yc - h/2)
+                        y2 = int(yc + h/2)
+                        lp_cropped = ori_frame[y1:y2, x1:x2, :]
+                        try:
+                            origin = save_folder_tag[i].split("/")[-1]
+                            cv2.imwrite(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg", lp_cropped)
+                            # print(f"{save_folder_tag[i]}/{origin}_{img_cnt}_{lp_cnt}.jpg")
+                        except:
+                            pass
+                            # print("failed")
+                        lp_cnt += 1
+
+                img_cnt += 1
+            except:
+                time.sleep(1)
+                print('failed')
